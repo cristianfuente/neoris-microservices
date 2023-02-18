11bd50028940944ed9b3cfd167d00da25e518893
cristian <cristiandavidfuentes@hotmail.com>
2023-02-18 15:56:03 -0500

    create models

---
+++ customer/src/main/java/com/neoris/customer/model/Cliente.java
@@ -0,0 +1,24 @@
+package com.neoris.customer.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "CLIENTE", schema = "public")
+public class Cliente extends Persona{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String password;
+    @Column
+    private Boolean state;
+
+}
